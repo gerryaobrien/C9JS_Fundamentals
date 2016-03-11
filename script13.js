bbd50f92768f8a4dee69f8c05df0bbb427de78a4
Gerry O'Brien <gerry.obrien@microsoft.com>
2016-03-11 15:06:12 -0800

    using window.onLoad

--- script13.js
+++ script13.js
@@ -1,3 +1,10 @@
+window.onload = function(){
+   // var clickMeButton = document.getElementById('clickMe');
+    //clickMeButton.onclick = runTheExample();
+    
+    document.getElementById("clickMe").onclick = runTheExample;
+}
+
 
 function runTheExample() {
     alert("running the example");
