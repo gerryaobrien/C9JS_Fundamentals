d0761d5ab9ab9f5611b5f3dfa9d384ff799f7c40
Gerry O'Brien <gerry.obrien@microsoft.com>
2016-03-15 10:22:46 -0700

    wrote function

--- script13.js
+++ script13.js
@@ -5,7 +5,42 @@ window.onload = function(){
     document.getElementById("clickMe").onclick = runTheExample;
 }
 
-
+/*
 function runTheExample() {
     alert("running the example");
+}
+*/
+
+function runTheExample() {
+    /*
+    var myElement = document.getElementById('second');
+    var myNodeName = myElement.nodeName;
+    
+    alert(myNodeName);
+    
+    if(myElement != null){
+        alert(myElement.innerHTML);
+    }
+    
+    document.getElementById('second').innerHTML = "See how I set the text here?";
+    */
+    
+    //var listOfParagraphs = document.getElementsByTagName('p');
+    
+    //alert(listOfParagraphs.length);
+    
+    //var secondParagraph = listOfParagraphs[1];
+    
+    //alert(secondParagraph.innerHTML);
+    
+    /*
+    myElement = document.getElementById('second');
+    alert(myElement.parentNode.nodeName);
+    */
+    
+    var anchor = document.getElementById('myAnchor');
+    //var anchorDestination = anchor.href;
+    //alert(anchorDestination);
+    anchor.href = "http://www.learnvisualstudio.net";
+    
 }
